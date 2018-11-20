cb0d65d7b3a8e63352f6fc98b55b7665e9617b69
frapermor8 <44404440+frapermor8@users.noreply.github.com>
2018-11-20 16:08:58 +0100

    Update local_settings.travis.py

--- decide/local_settings.travis.py
+++ decide/local_settings.travis.py
@@ -30,9 +30,8 @@ BASEURL = 'http://10.5.0.1:8000'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'decide',
-        'PASSWORD': 'decide',
-        'USER': 'decide',
+        'NAME': 'postgres',
+        'USER': 'postgres',
         'HOST': 'localhost',
         'PORT': '5432',
     }
